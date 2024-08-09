0bc2bbd4b05e64ca5cb444d374ba00758813f258
Tony Menzo <tmenzo@login39.chn.perlmutter.nersc.gov>
2024-08-08 21:05:09 -0700

    fixed reweighting issue - updated flow map for psuedo chi2 loss

---
+++ ARRG/wasserstein_loss.py
@@ -0,0 +1,14 @@
+import torch
+import ot
+
+class WassersteinLoss(torch.nn.Module):
+    def __init__(self, p):
+        super(WassersteinLoss, self).__init__()
+        self.p = p
+
+    def forward(self, x, y, x_weights):
+        """
+        Compute the one-dimensional Wasserstein distance between two input tensors x and y
+        with x weighted by x_weights
+        """
+        return ot.wasserstein_1d(x, y, u_weights = x_weights, p = self.p)
